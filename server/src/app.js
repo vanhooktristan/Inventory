53117137d1732b5abc1bbe21355e61b8c2f490f7
Tristan <vanhooktristan@gmail.com>
2019-05-22 09:37:34 -0500

    morgan fix

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
